91efeb3f7bbe15fb0962d066e18183ec9b5d48c6
taylor-tllaw <tinlok@spacious.hk>
2022-06-13 13:55:17 +0800

    [*] update vgit keymap

--- lua/setup/vgit.lua
+++ lua/setup/vgit.lua
@@ -4,7 +4,9 @@ vgit.setup {
   keymaps = {
     ['n <C-k>'] = 'hunk_up',
     ['n <C-j>'] = 'hunk_down',
-    ['n <leader>gh'] = 'buffer_hunk_preview',
-    ['n <leader>gd'] = 'buffer_diff_preview',
+    ['n <leader>ghb'] = 'buffer_hunk_preview',
+    ['n <leader>gdb'] = 'buffer_diff_preview',
+    ['n <leader>ghp'] = 'project_hunks_preview',
+    ['n <leader>gdp'] = 'project_diff_preview',
   },
 }
